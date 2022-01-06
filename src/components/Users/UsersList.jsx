721b19cb655e69755522ac585e37c78add090e91
Irfan-Ansari-au25 <irfan398649@gmail.com>
2022-01-06 07:47:14 +0530

    added userList and Error Modal

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -7,11 +7,12 @@ const UsersList = (props) => {
   return (
     <Card className={classes.users}>
       <ul>
-        {props.users.map((user) => (
-          <li key={user.id}>
-            {user.name} ({user.age} years old)
-          </li>
-        ))}
+        {props.user &&
+          props.users.map((user) => (
+            <li key={user.id}>
+              {user.name} ({user.age} years old)
+            </li>
+          ))}
       </ul>
     </Card>
   );
